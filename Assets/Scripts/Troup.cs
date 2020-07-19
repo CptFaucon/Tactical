a80f62620f83666f39e90be9cb433724878b1179
Noé <noe.emery@gmail.com>
2020-07-19 22:36:24 +0200

    Troup Scripts test V1

---
+++ Assets/Scripts/Troup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+//comportement des troupes
+public class Troup : MonoBehaviour
+{
+    public bool isEnemy; //est-ce un allié ou un ennemi
+
+    Pathfinding pathfinding; //référence à Pathfinding
+    TroupData troupData; //référence à troupData
+
+    Transform troupPosition; //position de l'unité
+    SpriteRenderer troupSprite; //texture de l'unité
+
+    private void Start()
+    {
+        //on récupère toutes les références
+        pathfinding = GetComponentInParent<Pathfinding>();
+        troupData = GetComponentInParent<TroupData>();
+        troupSprite = GetComponent<SpriteRenderer>();
+
+        //si l'unité est un ennemi
+        if (isEnemy == true)
+        {
+            //on lui assigne la position d'arrivée
+            troupPosition = pathfinding.target;
+            //on lui assigne la texture d'un ennemi
+            troupSprite.sprite = troupData.EnemyTroup;
+        }
+
+        //si l'unité est alliée
+        else
+        {
+            //on lui assigne la position de départ
+            troupPosition = pathfinding.seeker;
+            //on lui assigne la texture d'un allié
+            troupSprite.sprite = troupData.AllyTroup;
+        }
+        //on réassigne la position de l'unité
+        transform.position = troupPosition.position;
+    }
+}
---
+++ Assets/Scripts/TroupData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroupData : MonoBehaviour
+{
+    public Sprite AllyTroup;
+    public Sprite EnemyTroup;
+}
